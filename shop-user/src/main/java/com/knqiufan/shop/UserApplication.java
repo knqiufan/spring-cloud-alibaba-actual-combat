5e489be0ca4db9deafb373c79f5dd22254b81026
knqiufan <knqiufan@foxmail.com>
2023-03-14 00:55:09 +0800

    user dir update -- nacos

--- shop-user/src/main/java/com/knqiufan/shop/UserApplication.java
+++ shop-user/src/main/java/com/knqiufan/shop/UserApplication.java
@@ -3,6 +3,7 @@ package com.knqiufan.shop;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -15,6 +16,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement(proxyTargetClass = true)
 @MapperScan(value = {"com.knqiufan.shop.user.mapper"})
+@EnableDiscoveryClient
 public class UserApplication {
 
     public static void main(String[] args) {

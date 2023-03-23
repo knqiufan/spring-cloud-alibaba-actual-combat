194757e7f882c975ccb92d6a4e14cc45e59b9ef0
knqiufan <knqiufan@foxmail.com>
2023-03-24 00:11:23 +0800

    gateway model add -- 新增服务网关模块

---
+++ shop-gateway/src/main/java/com/knqiufan/shop/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.knqiufan.shop.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 服务网关启动器
+ *
+ * @author 黄语秋
+ * @version 1.0.0
+ * @date 2023/3/24 0:07
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}

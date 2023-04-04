d6c3d4d54d0a409fdea8e5491ab550109f2ff291
knqiufan <knqiufan@foxmail.com>
2023-04-05 00:45:18 +0800

    user model update -- 用户新增api过滤测试接口

--- shop-user/src/main/java/com/knqiufan/shop/user/controller/UserController.java
+++ shop-user/src/main/java/com/knqiufan/shop/user/controller/UserController.java
@@ -6,9 +6,13 @@ import com.knqiufan.shop.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * User接口类
@@ -29,5 +33,14 @@ public class UserController {
         User user = userService.getUserById(uid);
         log.info("get user info: {}", JSONObject.toJSONString(user));
         return user;
+    }
+
+    @GetMapping("/api/apiFilter")
+    public String apiFilter(HttpServletRequest req, HttpServletResponse resp) {
+        log.info("访问了apiFilter接口");
+        String ip = req.getHeader("IP");
+        String name = req.getParameter("name");
+        log.info("ip:{}, name:{}", ip, name);
+        return "apiFilter";
     }
 }
